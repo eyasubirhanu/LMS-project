c5f573a03631d3b4f3d234f1255a7ab6c665c01f
eyasubirhanu <eyasubirhanu0@gmail.com>
2022-11-22 18:28:15 +0300

    add:webresponse model

---
+++ api/model/web-response.go
@@ -0,0 +1,8 @@
+package model
+
+type WebResponse struct {
+	Code   int         `json:"code"`
+	Status string      `json:"status"`
+	Token  string      `json:"token,omitempty"`
+	Data   interface{} `json:"data,omitempty"`
+}
