0c37868e905d4c784d99ec78a1531e6e61ced4ed
eyasubirhanu <eyasubirhanu0@gmail.com>
2022-11-22 18:26:58 +0300

    add:user model

---
+++ api/entity/user.go
@@ -0,0 +1,22 @@
+package entity
+
+import "time"
+
+type Users struct {
+	Id                int
+	Name              string
+	Username          string
+	Email             string
+	Password          string
+	Role              int
+	Phone             string
+	Gender            int
+	DisabilityType    int
+	Address           *string
+	Birthdate         string
+	Image             *string
+	Description       *string
+	EmailVerification time.Time
+	CreatedAt         time.Time
+	UpdatedAt         time.Time
+}
